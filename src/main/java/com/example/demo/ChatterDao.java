1f47622242c0cf34d8165e14d19de699a47b0cb0
victorv3@illinois.edu <victorv3@illinois.edu>
2021-06-18 19:34:38 -0500

    New Class to Chatter

---
+++ src/main/java/com/example/demo/ChatterDao.java
@@ -0,0 +1,32 @@
+package com.example.demo;
+
+public class ChatterDao {
+	
+	//private String name;
+	private String username;
+	private String group;
+	
+	/*public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}*/
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getGroup() {
+		return group;
+	}
+	public void setGroup(String group) {
+		this.group = group;
+	}
+	
+	
+	
+	
+
+}
